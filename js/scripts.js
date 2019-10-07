71b082f6ba052ac0161cc4b967afa6a39a4e1575
Kate Kelly <katefkelly18@gmail.com>
2019-10-07 11:18:23 -0700

    Return correct functions

--- js/scripts.js
+++ js/scripts.js
@@ -1,23 +1,23 @@
 //business logic//
 var beepBoop = function(userInput) {
   var numberArray = []
-for (index=0; index <= userInput; index++) {
+for (var index=0; index <= userInput; index++) {
   numberArray.push(index);
 }
-var results = numberArray.map(function(user)
+var results = numberArray.map(function(user) {
 var userToString = user.toString();
 if (userToString.indexOf(3) > - 1) {
   return "I'm sorry, Dave. I'm afraid I can't do that.";
-} else if (userToString.indexOf(2) > -1) {
+  } else if (userToString.indexOf(2) > -1) {
   return "Boop";
-} else if (userToString.indexOf(1) > -1) {
+  } else if (userToString.indexOf(1) > -1) {
   return "Beep";
-} else {
+  } else {
   return " " + user;
 }
 })
 
-return numberArray;
+return results;
 }
 
 //user interface logic//
